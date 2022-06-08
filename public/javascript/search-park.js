4b0a80ebe16e34df6196c136099b8c90fb452361
BrandonDaws <velarde.brandon02@gmail.com>
2022-06-08 13:48:22 -0500

    fixed fetch setup, user can now search b state code

--- public/javascript/search-park.js
+++ public/javascript/search-park.js
@@ -1,25 +1,35 @@
 const state = document.querySelector('input[name="search-park"]').value;
 const apiKey = "btd26H7hQ7f20oVkr4RANrbKwlgH9eyIk20pemcR"
+const stateName = document.getElementById("parks");
+
 
 const searchParkHandler = function () {
+  
+  console.log(stateName);
 
-    const apiUrl = "https://developer.nps.gov/api/v1/parks?stateCode=" + "TX" + "&api_key=" + apiKey;
+    const apiUrl = "https://developer.nps.gov/api/v1/parks?stateCode=" + stateName.value + "&api_key=" + apiKey;
 
     fetch(apiUrl)
     .then(function(response) {
         if(response.ok) {
             response.json().then(function(data) {
                 console.log(data);
-                var stateParks = [];
-                for (let i = 0; i < data.total; i++) {
-                    stateParks[i] = data.data[i].fullName;
-                    var stateParkName = document.createElement("p");
-                    stateParkName.textContent = stateParks[i];
-                    stateParkName.appendChild(stateParkName);
-                }
+                
+                parkResult(data);
             });
         }
     });
 }
 
+const parkResult = function(data){
+
+}
+/*
+var stateParks = [];
+for (let i = 0; i < data.total; i++) {
+    stateParks[i] = data.data[i].fullName;
+    var stateParkName = document.createElement('div');
+    stateParkName.textContent = stateParks[i];
+    parkRes.append(stateParkName)
+    */
 document.querySelector('.search-park-btn').addEventListener('click', searchParkHandler);
